8894cf57e6806bf3497c63a087b0c4504957194e
yang.ding <yang.ding@fraudmetrix.cn>
2014-10-21 16:12:27 +0800

    test

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('我爱北京天安门\n');
+	res.end('我爱北安门\n');
 }).listen(process.env.PORT, process.env.IP);
 console.log('Server running at http://'+process.env.IP+':'+process.env.PORT+'/');
